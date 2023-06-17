bcdbb00fa6dda1a9ee49363fa758a640e777fc13
Cássio Marcião <marciaocassio@gmail.com>
2023-06-17 19:28:41 -0300

    feat: Add header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,33 @@
+import { styled } from "styled-components";
+
+export const Container = styled.header`
+    height: 198px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: #D73035;
+`;
+
+export const Content = styled.div`
+    max-width: 1216px;
+    width: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    color: #fafafa;
+
+    h1 {
+        font-size: 2rem;
+    }
+
+    h2 {
+        font-size: 1rem;
+        font-weight: 400;
+
+        margin-top: 6px;
+    }
+`;
