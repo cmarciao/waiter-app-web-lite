2f4dc92fc4c1ff4b25091e07d7bfc6222d292591
Cássio Marcião <marciaocassio@gmail.com>
2023-06-18 02:47:44 -0300

    feat: Add OrdersBoard component

---
+++ src/components/OrdersBoard/index.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Order } from "../../types/Order";
+import { OrderModal } from "../OrderModal";
+import { Container, OrdersContainer } from "./styles";
+
+interface OrdersBoardProps {
+    icon: string;
+    title: string;
+    orders: Order[];
+}
+
+export function OrdersBoard({ icon, title, orders }: OrdersBoardProps) {
+    const [isVisibleOrderModal, setIsVisibleOrderModal] = useState(false);
+    const [selectedOrder, setSelectedOrder] = useState<null | Order>(null);
+
+    function handleOpenOrderModal(order: Order) {
+        setIsVisibleOrderModal(true);
+        setSelectedOrder(order);
+    }
+
+    function handleCloseOrderModal() {
+        setIsVisibleOrderModal(false);
+        setSelectedOrder(null);
+    }
+
+    return (
+        <Container>
+            <header>
+                <span>{icon}</span>
+                <strong>{title}</strong>
+                <span>({orders.length})</span>
+            </header>
+
+            {orders.length > 0 && (
+                <OrdersContainer>
+                {orders.map((order) => (
+                    <button
+                        key={order._id}
+                        type="button"
+                        onClick={() => handleOpenOrderModal(order)}
+                    >
+                        <strong>{order.table}</strong>
+                        <span>{order.products.length} itens</span>
+                    </button>
+                ))}
+            </OrdersContainer>
+            )}
+
+            <OrderModal
+                isVisible={isVisibleOrderModal}
+                order={selectedOrder}
+                onClose={handleCloseOrderModal}
+            />
+
+        </Container>
+    )
+}
---
+++ src/components/OrdersBoard/styles.ts
@@ -0,0 +1,60 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    padding: 1rem;
+    border-radius: 1rem;
+    border: 1px solid rgba(204, 204, 204, 0.8);
+
+    & > header {
+        display: flex;
+        align-content: center;
+
+        font-size: 0.875rem;
+        gap: .5rem;
+
+        padding: .5rem;
+    }
+    `;
+
+export const OrdersContainer = styled.div`
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+
+    margin-top: 1.5rem;
+
+    button {
+        height: 8rem;
+        width: 100%;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        gap: .25rem;
+        background: #fff;
+
+        border-radius: .5rem;
+        border: 1px solid rgba(204, 204, 204, 0.8);
+
+        strong {
+            font-weight: 500;
+        }
+
+        span {
+            color: #666;
+            font-size: .875rem;
+        }
+
+        & + button {
+            margin-top: 1.5rem;
+        }
+    }
+`;
