e5c6323b09e08c5ead71f9337877edcc91a43d01
Cássio Marcião <marciaocassio@gmail.com>
2023-06-18 02:47:59 -0300

    feat: Add Orders component

---
+++ src/components/Orders/index.tsx
@@ -0,0 +1,55 @@
+import { OrdersBoard } from "../OrdersBoard";
+import { Order } from "../../types/Order";
+import { Container } from "./styles";
+
+const mockedOrders: Order[] = [
+    {
+      _id: '6372e48cbcd195b0d3d0f7f3',
+      table: '123',
+      status: 'WAITING',
+      products: [
+        {
+          product: {
+            name: 'Pizza quatro queijos',
+            imagePath: '1686974981424-quatro-queijos.png',
+            price: 40,
+          },
+          quantity: 3,
+          _id: '6372e48cbcd195b0d3d0f7f4'
+        },
+        {
+          product: {
+            name: 'Coca cola',
+            imagePath: '1686974981424-quatro-queijos.png',
+            price: 7,
+          },
+          quantity: 2,
+          _id: '6372e48cbcd195b0d3d0f7f5'
+        }
+      ],
+    }
+  ];
+
+export function Orders() {
+    return (
+        <Container>
+            <OrdersBoard
+                icon="🕕"
+                title="Fila de espera"
+                orders={mockedOrders}
+                />
+
+            <OrdersBoard
+                icon="👨‍🍳"
+                title="Em preparação"
+                orders={[]}
+                />
+
+            <OrdersBoard
+                icon="✅"
+                title="Pronto"
+                orders={[]}
+            />
+        </Container>
+    );
+}
---
+++ src/components/Orders/styles.ts
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+    max-width: 1216px;
+    width: 100%;
+
+    display: flex;
+    gap: 2rem;
+
+    margin: 2.5rem auto;
+`;
